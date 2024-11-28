74e0f34373b14fad276724702618ba2ec59da3dc
Piyush patil <098piyushpatil@gmail.com>
2024-11-29 03:20:27 +0530

    Frontend completed

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './chat.css'
+import EmojiPicker from 'emoji-picker-react'
+
 
 const Chat = () => {
+  const [open, setOpen] = useState(false)
+  const [text, setText] = useState("")
+
+  const handleEmoji = (e) => {
+    setText((prev)=> prev + e.emoji)
+    setOpen(false)
+  }
+
+  const endRef = useRef(null)
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({behavior:"smooth"})
+  }, [])
+  
+  
+
   return (
     <div className="chat">
       <div className="top">
         <div className="user">
           <img src="./avatar.png" alt="" />
           <div className="texts">
-            <span>Jane Doe</span>
+            <h3>John Doe</h3>
             <p>Lorem ipsum dolor sit.</p>
           </div>
         </div>
@@ -18,8 +36,87 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
-      <div className="bottom"></div>
+
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing 
+              elit. Molestiae quae sapiente nemo, praesentium ex dolorem 
+              eligendi quisquam inventore deleniti. Commodi minima assumenda 
+              sapiente voluptatum dsoluta. Nostrum sequi repellendus accusamus
+              laboriosam?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="./mic.png" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing 
+              elit. Molestiae quae sapiente nemo, praesentium ex dolorem 
+              eligendi quisquam inventore deleniti. Commodi minima assumenda 
+              sapiente voluptatum dsoluta. Nostrum sequi repellendus accusamus
+              laboriosam?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing 
+              elit. Molestiae quae sapiente nemo, praesentium ex dolorem 
+              eligendi quisquam inventore deleniti. Commodi minima assumenda 
+              sapiente voluptatum dsoluta. Nostrum sequi repellendus accusamus
+              laboriosam?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+          <img src="./avatar.png" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing 
+              elit. Molestiae quae sapiente nemo, praesentium ex dolorem 
+              eligendi quisquam inventore deleniti. Commodi minima assumenda 
+              sapiente voluptatum dsoluta. Nostrum sequi repellendus accusamus
+              laboriosam?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div ref={endRef}></div>
+      </div>
+
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input 
+          type="text"
+          value={text} 
+          onChange={(e)=>setText(e.target.value)}
+          placeholder='Type a message....' 
+        />
+        <div className="emoji">
+          <img 
+            src="./emoji.png" 
+            alt=""
+            onClick={()=>setOpen((prev)=>!prev)}  
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
     </div>
   )
 }
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -4,7 +4,76 @@ import './detail.css'
 const Detail = () => {
   return (
     <div className='detail'>
-      detail
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem, ipsum dolor lorem</p>
+      </div>
+      
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photo</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+        </div>
+        <div className="photos">
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img src="./mic.png" alt="" />
+              <span>photo2024.png</span>
+            </div>
+            <img src="./download.png" alt=""  className='icon'/>
+          </div>
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img src="./mic.png" alt="" />
+              <span>photo2024.png</span>
+            </div>
+            <img src="./download.png" alt=""  className='icon' />
+          </div>
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img src="./mic.png" alt="" />
+              <span>photo2024.png</span>
+            </div>
+            <img src="./download.png" alt=""  className='icon' />
+          </div>
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img src="./mic.png" alt="" />
+              <span>photo2024.png</span>
+            </div>
+            <img src="./download.png" alt=""  className='icon'/>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className='logout'>Log out</button>
+      </div>
     </div>
   )
 }
